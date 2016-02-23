c8f319247042afff2d2da3ad342a3f145708733d
Fabricio Carrara <fabricio_x@hotmail.com>
2016-02-24 00:07:13 +0500

    Improve problem 3

--- scripts/problems.js
+++ scripts/problems.js
@@ -17,75 +17,94 @@ document.getElementById("problem_1_button").onclick = function(){
 //Problem 2 - Even Fibonacci numbers
 document.getElementById("problem_2_button").onclick = function(){
     
-	var x = 0;
-	var y = 0;
-	var z = 0;
-	var sum = 0;
-	var n = document.getElementById("problem_2_input").value;
+  var x = 0;
+  var y = 0;
+  var z = 0;
+  var sum = 0;
+  var n = document.getElementById("problem_2_input").value;
 
-	function fibonacci(y){
+  function fibonacci(y){
 
-	  z = x + y;
+    z = x + y;
 
-	  if (z < n){
+    if (z < n){
 
-	    x = y;
+      x = y;
 
-	    if (z % 2 !== 0){
-	      sum += z;
-	    }
-	    
-	    fibonacci(z);
-	    
-	  } else{
-	    return;
-	  }
-	}
+      if (z % 2 !== 0){
+        sum += z;
+      }
+      
+      fibonacci(z);
+      
+    } else{
+      return;
+    }
+  }
 
-	fibonacci(1);
-	document.getElementById("problem_2_answer").innerHTML = sum;
+  fibonacci(1);
+  document.getElementById("problem_2_answer").innerHTML = sum;
 
 };
 
-//Problem 3 - Largest prime factor
+//Problem 3 - Largest prime factor (efficient)
 document.getElementById('problem_3_button').onclick = function(){
-
-  function isPrime(num){
-    
-    for (var j=2; j < num; j++){
-      if (num % j === 0){
-        return false;
-      }
-    }
-    
-    return true;
-  }
 
   function largestPrimeFactor(num){
     
-    var found = false;
-    var i = 2;
-    var value;
-    
-    while (!found){
-    
-      value = num / i;
-      if (Number.isInteger(value)){
-        if (isPrime(value)){
-          found = true;
-        } else {
-          i++;
-        } 
-      } else {
-        i++;
-      }
+    var factor = 2;
+    while (num > factor){
+        if (num % factor === 0){
+            num = num / factor;
+            factor = 2;
+        } else{
+            factor++;
+        }
     }
-
-    document.getElementById('problem_3_answer').innerHTML = value;
+    
+    document.getElementById('problem_3_answer').innerHTML = factor;
   }
 
   largestPrimeFactor(document.getElementById('problem_3_input').value);
+
 };
+
+//Largest Prime Factor (inefficient)
+// function isPrime(num){
+  
+//   for (var j=2; j < num; j++){
+//     if (num % j === 0){
+//       return false;
+//     }
+//   }
+  
+//   return true;
+// }
+
+// function largestPrimeFactor(num){
+  
+//   var found = false;
+//   var i = 2;
+//   var value;
+  
+//   while (!found){
+  
+//     value = num / i;
+//     if (Number.isInteger(value)){
+//       if (isPrime(value)){
+//         found = true;
+//       } else {
+//         i++;
+//       } 
+//     } else {
+//       i++;
+//     }
+//   }
+
+//   document.getElementById('problem_3_answer').innerHTML = value;
+// }
+//   largestPrimeFactor(document.getElementById('problem_3_input').value);
+// };
 
 //Tabs logic
 var container = document.getElementById("tabs-container");

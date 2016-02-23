b2c52014a90a2d437d41ad7ed062200398e43075
Fabricio Carrara <fabricio_x@hotmail.com>
2016-02-23 18:26:23 +0500

    Finished first 3 problems

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# project-euler
+# Project Euler
+##Javascript solutions of the problems from the [Prject Euler](https://projecteuler.net) website.
+#####This is an assignment from [The Odin Project](http://www.theodinproject.com).
---
+++ scripts/problems.js
@@ -0,0 +1,115 @@
+//Problem 1 - Multiples of 3 and 5
+document.getElementById("problem_1_button").onclick = function(){
+  
+  var total = 0;
+  var n = document.getElementById("problem_1_input").value;
+
+  for (var i = 0; i < n; i++){
+    if ((i % 3 === 0) || (i % 5 === 0)){
+      total += i;
+    }
+  }
+
+  document.getElementById("problem_1_answer").textContent = total;
+  
+};
+
+//Problem 2 - Even Fibonacci numbers
+document.getElementById("problem_2_button").onclick = function(){
+    
+	var x = 0;
+	var y = 0;
+	var z = 0;
+	var sum = 0;
+	var n = document.getElementById("problem_2_input").value;
+
+	function fibonacci(y){
+
+	  z = x + y;
+
+	  if (z < n){
+
+	    x = y;
+
+	    if (z % 2 !== 0){
+	      sum += z;
+	    }
+	    
+	    fibonacci(z);
+	    
+	  } else{
+	    return;
+	  }
+	}
+
+	fibonacci(1);
+	document.getElementById("problem_2_answer").innerHTML = sum;
+
+};
+
+//Problem 3 - Largest prime factor
+document.getElementById('problem_3_button').onclick = function(){
+
+  function isPrime(num){
+    
+    for (var j=2; j < num; j++){
+      if (num % j === 0){
+        return false;
+      }
+    }
+    
+    return true;
+  }
+
+  function largestPrimeFactor(num){
+    
+    var found = false;
+    var i = 2;
+    var value;
+    
+    while (!found){
+    
+      value = num / i;
+      if (Number.isInteger(value)){
+        if (isPrime(value)){
+          found = true;
+        } else {
+          i++;
+        } 
+      } else {
+        i++;
+      }
+    }
+
+    document.getElementById('problem_3_answer').innerHTML = value;
+  }
+
+  largestPrimeFactor(document.getElementById('problem_3_input').value);
+};
+
+//Tabs logic
+var container = document.getElementById("tabs-container");
+var tabs = container.querySelectorAll("ul li");
+
+function displayPage() {
+  var current = this.id;
+
+  var index = current.split("_")[1];
+
+  for (var i = 0; i < tabs.length; i++){
+    
+    if (tabs[i].classList.contains("selected")){
+      tabs[i].classList.remove("selected");
+    }
+
+    document.getElementById("problem_" + (i + 1)).style.display = "none";
+  }
+
+  document.getElementById(current).className = "selected";
+  document.getElementById("problem_" + index).style.display = "block";
+}
+
+for (var i = 0; i < tabs.length; i++) {
+  tabs[i].onclick = displayPage;
+}
+
